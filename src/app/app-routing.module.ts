66cb7be4d9310e525ad192dc95d7a85e24f1b1f3
xuanhuy <huynguyen@asimtelecom.vn>
2021-06-26 16:48:50 +0700

    sss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'first-component', component: FirstComponent},
+  {path: 'second-component', component: SecondComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
